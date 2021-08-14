dd6a6f4ac0e3b1a1b37ec15a6d6e17112887c11c
Sennarion <janadine.pro@gmail.com>
2021-08-14 10:33:30 +0300

    passed for verification

--- HW24/script.js
+++ HW24/script.js
@@ -1,19 +1,24 @@
-let promise = new Promise(function(resolve, reject){
-    let random = Math.floor(1 + Math.random() * 6);
-    setTimeout(function() {
-        if (1 <= random <= 5) {
-            console.log(`Start the game... ${random}`);
-        } else console.error(`Error`);
-    }, 2000)
-})
+function randomNumber() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let number = Math.floor(1 + Math.random() * 6);
+            if (1 <= number && number <= 5) {
+                console.log('Start the game...');
+                resolve(number);
+            } else reject('Game over');
+        }, 2000);
+    });
+}
 
-// const promise = new Promise((resolve, reject) => {
-//     const randomNumber = Math.random();
-//   setTimeout(() => {
-//       if(randomNumber < .6) {
-//         console.log('Все прошло отлично!');
-//       } else {
-//         reject('Что-то пошло не так');
-//     }
-//     }, 2000);
-//   });
+async function createGame() {
+    try {
+        let number = await randomNumber();
+        if (number == 1) {
+            console.log('Stay here');
+        } else console.log(`Go ${number} steps`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+createGame()
