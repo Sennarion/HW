4a048e60556194f7a0253afc890cf4a1b0c08c61
Sergey Reznichenko <janadine.pro@gmail.com>
2021-09-10 20:51:49 +0300

    done

--- HW34/src/components/UI/button/LikeButton.jsx
+++ HW34/src/components/UI/button/LikeButton.jsx
@@ -4,8 +4,10 @@ function LikeButton() {
     function changeColor(event) {
         if (event.target.style.background === 'red') {
             event.target.style.background = 'rgb(255, 162, 162)';
+            event.target.innerHTML = 'Like';
         } else {
             event.target.style.background = 'red';
+            event.target.innerHTML = 'Dislike';
         }
     };
 
